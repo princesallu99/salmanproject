0e4b5f2883d13d9043d9cd7c3e2733de0e8a8707
unknown <sjaved@W-260-03.dtcc.edu>
2015-10-22 09:39:44 -0400

    Initial commit

---
+++ P1/oct22a.py
@@ -0,0 +1,9 @@
+__author__ = 'sjaved'
+
+try:
+    reply = -1
+    while reply != 0:
+        reply = int(input("Enter a positive integer, zero when done: "))
+        print("Your input doubled is", reply*2)
+except:
+        print('You did not enter an integer')
